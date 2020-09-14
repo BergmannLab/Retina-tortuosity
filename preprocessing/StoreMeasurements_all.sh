673935daf0537e83f20f3c284c600a3fce6f0a8b
mattiat <mattia.tomasoni@unil.ch>
2020-09-14 18:30:29 +0200

    StoreMeasurements removing some debugging code

--- preprocessing/StoreMeasurements_all.sh
+++ preprocessing/StoreMeasurements_all.sh
@@ -29,8 +29,6 @@ mkdir $backup_dir
 
 #tot_png_files=$(find $raw_data_dir -name *png | wc -l) # count the number of raw input inputs
 #tot_mat_files=$(find $input_dir -name *_stats.tsv | wc -l) # count the number of measured images
-tot_png_files=100
-tot_mat_files=200
 echo "$tot_mat_files measurements"
 echo "$tot_png_files original raw images"
 
