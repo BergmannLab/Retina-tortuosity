4199b475188834dc84926e357c3c3f0dfdf79183
Philippe Jacquet <pjacquet@pulse-6-195.unil.ch>
2020-11-11 08:22:28 +0100

    Use cpt05; batch=1024

--- DL/helpers/05/TrainDL.py
+++ DL/helpers/05/TrainDL.py
@@ -65,7 +65,7 @@ def TrainDL(db_dir, gpuid, output_dir):
     drop_rate=0
     num_classes=2
     # --- training params
-    batch_size=128
+    batch_size=1024 #128
     patch_size=224 #currently, this needs to be 224 due to densenet architecture
     num_epochs = 4
     phases = ["train", "val"] #how many phases did we create databases for?
