0646ed7820275777ba71b00c2d6b0850c88f9285
Philippe Jacquet <pjacquet@pulse-7-69.unil.ch>
2020-12-03 10:16:58 +0100

    ROC on Retina data

--- DL/helpers/utils/BuildTestDatasetHypertension/BuildTestDatasetHypertension.sh
+++ DL/helpers/utils/BuildTestDatasetHypertension/BuildTestDatasetHypertension.sh
@@ -13,7 +13,7 @@
 source $HOME/retina/configs/config.sh
 
 #number of cases/controls
-limit=11500
+limit=11000
 
 # clear previous run
 output_dir=$scratch/retina/DL/output/utils/BuildTestDatasetHypertension/
