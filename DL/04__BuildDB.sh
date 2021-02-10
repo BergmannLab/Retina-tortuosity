4858e86bd48c193d099b357bc0e928e47ac469d0
Philippe Jacquet <pjacquet@pulse-171.unil.ch>
2021-02-10 10:37:13 +0100

    New dataset

--- DL/04__BuildDB.sh
+++ DL/04__BuildDB.sh
@@ -25,8 +25,8 @@ source $HOME/retina/configs/config.sh
 # CHOOSE DISEASE
 #input_dir=$scratch/retina/DL/output/utils/old_BuildTestDatasetHypertension/
 #
-input_dir=$scratch/retina/DL/output/utils/BuildDatasetAngina/
-#input_dir=$scratch/retina/DL/output/utils/BuildDatasetDiabetes
+#input_dir=$scratch/retina/DL/output/utils/BuildDatasetAngina/
+input_dir=$scratch/retina/DL/output/utils/BuildDatasetDiabetes
 #input_dir=$scratch/retina/DL/output/utils/BuildDatasetDV_Thrombosis/
 #input_dir=$scratch/retina/DL/output/utils/BuildDatasetHeart_Attack/
 #input_dir=$scratch/retina/DL/output/utils/old_BuildDatasetHypertension/
