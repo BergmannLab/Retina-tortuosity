3033f05b81e5d80b5d22d54cf7c599e649425f59
Philippe Jacquet <pjacquet@pulse-13-39.unil.ch>
2021-02-11 13:50:05 +0100

    New dataset

--- DL/04__BuildDB.sh
+++ DL/04__BuildDB.sh
@@ -28,12 +28,12 @@ source $HOME/retina/configs/config.sh
 #input_dir=$scratch/retina/DL/output/utils/BuildDatasetAngina/
 #input_dir=$scratch/retina/DL/output/utils/BuildDatasetDiabetes
 #input_dir=$scratch/retina/DL/output/utils/BuildDatasetDV_Thrombosis/
-#input_dir=$scratch/retina/DL/output/utils/BuildDatasetHeart_Attack/
+input_dir=$scratch/retina/DL/output/utils/BuildDatasetHeart_Attack/
 #input_dir=$scratch/retina/DL/output/utils/old_BuildDatasetHypertension/
 #input_dir=$scratch/retina/DL/output/utils/BuildDatasetStroke/
 #
-# The Kanji dataset to check if the code works well
-input_dir=$scratch/retina/DL/output/utils/Kanji/
+# The Kanji dataset may be used to make sure that the pipeline works well, i.e. gives 100% validation accuracy
+#input_dir=$scratch/retina/DL/output/utils/Kanji/
 
 
 controls_dir=$input_dir/controls/
