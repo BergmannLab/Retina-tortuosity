533c6d3af994c78667d77162bb61846623dc7308
Alexander Luke Button <abutton@frt.chuv.vital-it.ch>
2021-04-13 16:13:48 +0200

    updated ExtractFeature to combine train and validation data

--- DL/helpers/05/features/ExtractFeature.py
+++ DL/helpers/05/features/ExtractFeature.py
@@ -19,10 +19,27 @@ import sys
 sys.path.insert(1, '../')
 from TrainDL import TrainParameters,ImageProcess,Dataset
 
-#set train parametera
+#feature extractoin functions
+def abs_max(sample):
+	return [np.max(abs(sample[-1][-1].detach().numpy()))]
+
+def ave(sample):
+	return [np.average(sample[-1][-1].detach().numpy())]
+
+def flat_layer(sample):
+	layer = sample[-1][-1].detach().numpy().flatten()
+	return layer
+	
+#set feature_func
+feature_func = flat_layer
+
+#set output file
+output_file = open("last_layer.out","w+")
+
+#set train parameters
 tp = TrainParameters()
 
-device = torch.device(f'cuda:{gpuid}' if torch.cuda.is_available() else 'cpu')
+device = 'cpu'#torch.device(f'cuda:{gpuid}' if torch.cuda.is_available() else 'cpu')
 D = DenseNet(growth_rate=tp.growth_rate,
             block_config=tp.block_config,
             num_init_features=tp.num_init_features,
@@ -36,32 +53,36 @@ state_dict = torch.load(dict_path)["model_dict"]
 missing_keys = D.load_state_dict(state_dict)
 
 #set image processing methods
-data_path = "/scratch/beegfs/FAC/FBM/DBC/sbergman/retina/DL/output/04_DB/retina_train.pytable"
-im_pro = ImageProcess()
-im_pro.set_norm_img_transform(data_path)
+data_label_list = ["train","val"]
+write_header=True
+for data_idx,data_label in enumerate(data_label_list):
+	data_path = "/scratch/beegfs/FAC/FBM/DBC/sbergman/retina/DL/output/04_DB/retina_%s.pytable"%(data_label,)
+	im_pro = ImageProcess()
+	im_pro.set_norm_img_transform(data_path)
 
-#load dataset
-dataset=Dataset(data_path, img_transform=im_pro.norm_transform_train)
-dataLoader=DataLoader(dataset, batch_size=tp.batch_size, num_workers=16, pin_memory=True)
+	#load dataset
+	dataset=Dataset(data_path, img_transform=im_pro.norm_transform_train)
+	dataLoader=DataLoader(dataset, batch_size=tp.batch_size, num_workers=16, pin_memory=True)
 
-#load patient ids
-pytables_data = tables.open_file(data_path,'r')
-patient_id = pytables_data.root.filenames.read()
-pytables_data.close()
+	#load patient ids
+	pytables_data = tables.open_file(data_path,'r')
+	patient_id = pytables_data.root.filenames.read()
+	pytables_data.close()
 
-output_file = open("test.out","w+")
-index=0
-for ii , (X, label, img_orig) in enumerate(dataLoader):
-	X = X.to(device)  # [Nbatch, 3, H, W]
-	label = label.type('torch.LongTensor').to(device)  # [Nbatch, 1] with class indices (0, 1, 2,...n_classes)
-	
-	#print(patient_id[ii])
-	#print(np.max(abs(D.features(X)[0][0].detach().numpy())))
+	index=0
+	for ii , (X, label, img_orig) in enumerate(dataLoader):
+		X = X.to(device)  # [Nbatch, 3, H, W]
+		label = label.type('torch.LongTensor').to(device)  # [Nbatch, 1] with class indices (0, 1, 2,...n_classes)
 
-	F=D.features(X)
-	for sample in F:
-		p_id = str(patient_id[index]).split("/")[-1].split("_")[0]
-		max_val = np.average(sample[-1][-1].detach().numpy())
-		output_file.write(p_id+","+str(max_val)+"\n")
-		index = index + 1
+		F=D.features(X)
+		for sample in F:
+			p_id = str(patient_id[index]).split("/")[-1].split("_")[0]
+			feature_value = feature_func(sample)
+			feature_line = ",".join([str(f) for f in feature_value])
+			if write_header:
+				feature_header = ",".join(["Feature %d"%(fid,) for fid in range(len(feature_value))])
+				output_file.write("Patient ID,"+feature_header+", Dataset\n")
+				write_header=False
+			output_file.write(p_id+","+feature_line+","+data_label+"\n")
+			index = index + 1
 output_file.close()
