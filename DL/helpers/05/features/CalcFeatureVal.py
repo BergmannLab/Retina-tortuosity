8ca554e0574d46903494f10c8daba5b0b584a050
Alexander Luke Button <abutton@frt.chuv.vital-it.ch>
2021-04-26 18:49:17 +0200

    results for 4th layer

--- DL/helpers/05/features/CalcFeatureVal.py
+++ DL/helpers/05/features/CalcFeatureVal.py
@@ -15,19 +15,35 @@ def ave(layer_activation):
 def flat_layer(layer_activation):
 	return layer_activation.flatten()
 
+def extract_node_value(layer_activation,node_id):
+	return [layer_activation.flatten()[node_id]]
 
-output_file = open("ave_test.out","w+")
+def load_node_id_list(node_file):
+	n_object = open(node_file,"r")
+	header = n_object.readline()
+
+	node_id = []
+	for line in n_object:
+		node_id.append(int(line.strip("\n").split(",")[1]))
+	return node_id
+
+node_id = load_node_id_list("analysis/max_mean_node_intensity.csv")
+selected_id = 3
+
+output_file = open("output/node_id_test_layer_%d.out"%(node_id[selected_id],),"w+")
 output_file.write("Subject ID,feature value\n")
 
 layer_dir = "/scratch/beegfs/FAC/FBM/DBC/sbergman/retina/DL/output/features/"
-extracted_layers = os.listdir(layer_dir)
+extracted_layers = [l for l in os.listdir(layer_dir) if "feature_0" in l]
 
 st = time.time()
 for layer_idx,layer_file in enumerate(extracted_layers):
-        layer = np.load(layer_dir+"/"+layer_file)
+	if layer_idx % 100 == 0:
+		print(layer_idx,"processed")
+	layer = np.load(layer_dir+"/"+layer_file)
         subject_id = layer_file.split("_")[0]
 
-        feature_value = ave(layer)[0]
+	feature_value = extract_node_value(layer[0][0],node_id[selected_id])[0]
         output_file.write(subject_id+","+str(feature_value)+"\n")
 print("time taken :",time.time()-st)
 output_file.close()
