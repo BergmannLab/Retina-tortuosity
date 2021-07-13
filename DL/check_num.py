3ffa2d88be7fb1c0e19ef20c0f18f4d28de2b635
Alexander Luke Button <abutton@frt.chuv.vital-it.ch>
2021-07-13 10:55:02 +0200

    compare roc curvers

--- DL/check_num.py
+++
@@ -1,9 +0,0 @@
-import os
-import numpy as np
-
-f_object = open("filenames_1.txt","r")
-data = []
-for line in f_object:
-	data.append(line)
-f_object.close()
-print(len(data))
--- DL/helpers/05/features/roc_curve.py
+++ DL/helpers/05/features/roc_curve.py
@@ -58,6 +58,7 @@ for data_idx,data_label in enumerate(data_label_list):
 	correct_val = []	
 
 	for ii , (img, label, img_orig) in enumerate(dataLoader):
+		print(label)
 		if ii % 100 == 0:
 			print(ii,"images processed")
 		img = img.to(device)  # [Nbatch, 3, H, W]
