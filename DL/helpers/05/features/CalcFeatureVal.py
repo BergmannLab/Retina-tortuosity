22d759fdac5f94c2c16b8aed78f880f92a57b817
Alexander Luke Button <alexanderluke.button@unil.ch>
2021-07-23 16:55:30 +0200

    updated CalcFeatureVal to extract last layer nodes

--- DL/helpers/05/features/CalcFeatureVal.py
+++ DL/helpers/05/features/CalcFeatureVal.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 import time
 import math
-import os 
+import os
 import matplotlib.pyplot as plt
 from sklearn.decomposition import pca
 import pickle
@@ -30,6 +30,12 @@ def extract_node_value(layer_activation,node_id):
 def extract_node_value_coord(layer_activation,node_coord):
         return [layer_activation[node_coord[0]][node_coord[1]]]
 
+def extract_nodes_ave_channels(layer_activation):
+	node_active = []
+	for layer_channel in layer_activation:
+		node_active.append(layer_channel.flatten())
+	return np.average(node_active,axis=0)
+
 def load_node_id_list(node_file):
 	n_object = open(node_file,"r")
 	header = n_object.readline()
@@ -50,7 +56,8 @@ dict_keys = list(pickle.load(open(layer_dir+"/"+extracted_layers[0],"rb")).keys(
 feature_header = ",".join(dict_keys)
 
 output_file = open("output/ave_across_channel_all_features.out","w+")
-output_file.write("Subject ID,%s,Dataset\n"%(feature_header,))
+#output_file.write("Subject ID,%s,Dataset\n"%(feature_header,))
+output_file.write("Subject ID,"+",".join(["node_"+str(i) for i in range(49)])+",Dataset\n")
 
 st = time.time()
 for layer_idx,layer_file in enumerate(extracted_layers):
@@ -60,9 +67,10 @@ for layer_idx,layer_file in enumerate(extracted_layers):
 	subject_id = layer_file.split("_")[0]
 	data_label = layer_file.split("_")[1]
 
-	feature_value = []
-	for feature_key in dict_keys:
-		feature_value.append(str(ave_across_channels(layer_dict[feature_key])[0]))
+	#feature_value = []
+	#for feature_key in dict_keys:
+	#	feature_value.append(str(ave_across_channels(layer_dict[feature_key])[0]))
+	feature_value = np.asarray(extract_nodes_ave_channels(layer_dict["feature_11"][0]),dtype='str')
 	output_file.write(subject_id+","+",".join(feature_value)+","+data_label+"\n")
 print("time taken :",time.time()-st)
 output_file.close()
