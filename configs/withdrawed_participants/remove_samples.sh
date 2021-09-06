1c2d5279f37e848a15be3db26564a3f35b1fe319
mbeyele5 <michael.beyeler@unil.ch>
2021-09-06 18:17:35 +0200

    withdrawed participants removed, corresponding script debugged

--- configs/withdrawed_participants/remove_samples.sh
+++ configs/withdrawed_participants/remove_samples.sh
@@ -7,20 +7,25 @@ echo Removing files corresponding to participant that have withdrawn consents
 
 function remove_files(){
   input=$1
-  dos2unix server_list.txt
   echo Processing file $input
   while IFS=\t read -r eid
   do
-    echo "Removing files for participant" "$eid"
-    rm "$data_dir"/"$eid"_21015_0_0.png
-    rm "$data_dir"/"$eid"_21016_0_0.png
-    rm "$data_dir"/"$eid"_21015_1_0.png
-    rm "$data_dir"/"$eid"_21016_1_0.png
+    echo "Removing fundus images for participant" "$eid"
+    
+    # rm -f "$data_dir"/"$eid"* # this was too slow, probably because it has to search through huge image directory each time
+    
+    for i in 21015 21016 ; do
+        for j in 0 1 ; do
+            for k in 0 1; do
+                rm -f "$data_dir"/"$eid"_"$i"_"$j"_"$k".png;
+            done
+        done
+    done
   done < $input
 }
 
-##remove_files "w43805_20200204.csv" #DONE
-#remove_files "w43805_20200820.csv" #DONE
-remove_files "w43805_20210809.csv"
+remove_files "w43805_20200204.csv" #DONE
+remove_files "w43805_20200820.csv" #DONE
+remove_files "w43805_20210809.csv" #DONE
 
 echo Done.
