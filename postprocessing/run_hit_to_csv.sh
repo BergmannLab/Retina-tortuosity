5d4bf61533948c79e19b88ee8b9dd485c0d35aa3
mbeyele5 <mjbeyeler@protonmail.ch>
2022-07-15 01:00:47 +0200

    hit_to_csv: Rebuilt from scratch in python. Speedup by factor 24.Now takes 1h instead of 1d when extracting ~100 traits

--- postprocessing/run_hit_to_csv.sh
+++ postprocessing/run_hit_to_csv.sh
@@ -1,35 +1,45 @@
 #!/bin/bash
-#BATCH --account=sbergman_retina
-#SBATCH --job-name=hitToCSV
-#SBATCH --nodes 1
-#SBATCH --ntasks 1
-#SBATCH --cpus-per-task 30
-#SBATCH --mem 500G
-#SBATCH --time 24:00:00
-#SBATCH --partition normal
-#####SBATCH --array=1
-
-
 
 # HOW-TO
-
-# sbatch hit_to_csv.sh *EXPERIMENT_ID* 
-
-
+# bash hit_to_csv.sh *EXPERIMENT_ID* 
 
 source /dcsrsoft/spack/bin/setup_dcsrsoft
-module load gcc r
-
+module load gcc python/3
 
 source ../configs/config.sh
 
-output_dir="$GWAS_DIR"/$1/
-echo $output_dir
+OUT_DIR="$GWAS_DIR"/$1/
+echo $OUT_DIR
+
+for i in "$OUT_DIR"/*.gz; do gunzip -f $i; done
+
+NUMBER_OF_PHENOTYPES=$(awk -F' ' '{print NF; exit}' $OUT_DIR/sample_sizes.txt)
+
+echo Number of phenotypes: $NUMBER_OF_PHENOTYPES
+
+cpu=1
+mem=600M #ok for full bgenie gwas
+time="01:00:00"
+
+for i in $(seq 1 $NUMBER_OF_PHENOTYPES); do
+	PHENO=$(cat $OUT_DIR/sample_sizes.txt | head -n1 | cut -f $i -d' ')
+	SAMPLE_SIZE=$(cat $OUT_DIR/sample_sizes.txt | tail -n1 | cut -f $i -d' ')
+	sbatch -p cpu -J hit_to_csv --mem $mem -t $time -c $cpu -N 1 --account sbergman_retina --partition normal --wrap "python3 -u hit_to_csv.py $OUT_DIR $PHENO $SAMPLE_SIZE"
+done
 
 
-for i in "$output_dir"/*.gz; do gunzip -f $i; done
 
-Rscript ./hit_to_csv.R $output_dir
+# showcase of brain being stuck
 
-
-
+#for i in $(seq 1 $PHENOTYPES_PER_ROUND $NUMBER_OF_PHENOTYPES); do
+#	NAIVE_LOOP_MAX=$(($i + $PHENOTYPES_PER_ROUND - 1))
+#	ACTUAL_LOOP_MAX=$(( NUMBER_OF_PHENOTYPES < NAIVE_LOOP_MAX ? NUMBER_OF_PHENOTYPES : NAIVE_LOOP_MAX ))
+#	for j in $(seq $i $ACTUAL_LOOP_MAX); do
+#		echo Phenotype number $j
+#		PHENO=$(cat $OUT_DIR/sample_sizes.txt | head -n1 | cut -f $j -d' ')
+#		SAMPLE_SIZE=$(cat $OUT_DIR/sample_sizes.txt | tail -n1 | cut -f $j -d' ')
+#		echo $PHENO $SAMPLE_SIZE
+#		python3 hit_to_csv.py $OUT_DIR $PHENO $SAMPLE_SIZE &
+#	done
+#	wait
+#done
